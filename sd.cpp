f1d77db6dcaa5ea24442f02986997bca8245f78e
Xtronix90 <sujaljainedu2002@gmail.com>
2023-04-01 17:55:45 +0530

    second commit

---
+++ sd.cpp
@@ -0,0 +1,29 @@
+//  Sujal Jain  2112090  //
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+#endif
+
+
+#define pb          push_back
+#define mp          make_pair
+#define FAST        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll          long long 
+#define ld          long double 
+
+int main() {
+    FAST
+        ll t;cin>>t;
+        while (t--)
+        {
+            cout<< "hello";
+        }
+    return 0;
+}
